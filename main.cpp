4e3bdbc2941e84575f16519400f670650fd17621
David A Torres <davidatorres123@gmail.com>
2025-05-29 16:16:47 -0400

    Update main.cpp
    remove nonfunction move operation

--- main.cpp
+++ main.cpp
@@ -35,14 +35,13 @@ int main() {
         std::cout << "2.  Insert File\n";
         std::cout << "3.  Search for File\n";
         std::cout << "4.  Remove File\n";
-        std::cout << "5.  Move File\n";
-        std::cout << "6.  Display Directory Structure\n";
-        std::cout << "7.  Undo previous action\n";
-        std::cout << "8.  Display contents of a specified directory\n";
-        std::cout << "9.  Rename a directory\n";
-        std::cout << "10. Copy a file\n";
-        std::cout << "11. Sort a specified directory\n";
-        std::cout << "12. Exit\n";
+        std::cout << "5.  Display Directory Structure\n";
+        std::cout << "6.  Undo previous action\n";
+        std::cout << "7.  Display contents of a specified directory\n";
+        std::cout << "8.  Rename a directory\n";
+        std::cout << "9. Copy a file\n";
+        std::cout << "10. Sort a specified directory\n";
+        std::cout << "11. Exit\n";
         std::cout << "Enter your choice: ";
         std::cin >> choice;
 
@@ -79,39 +78,28 @@ int main() {
                 fileSystem.remove(filename);
                 break;
             case 5:
-                fileSystem.createBackup();
-                std::cout << "Enter source directory: ";
-                std::cin >> name;
-                std::cout << "Enter destination directory: ";
-                std::cin >> filename;
-                std::cout << "Enter file name: ";
-                std::cin >> filename;
-                isDir = false;
-                fileSystem.enqueueMove(name, destination, filename, isDir);
-                break;
-            case 6:
                 std::cout << std::endl << "Directory structure:" << std::endl;
                 fileSystem.displayDirectoryStructure();
                 break;
-            case 7:
+            case 6:
                 fileSystem.restoreBackup();
                 std::cout << "Previous structure restored." << std::endl;
                 break;
-            case 8:
+            case 7:
                 std::cout << "Enter desired directory: ";
                 std::cin >> dirName;
                 fileSystem.displayDirectoryContents(dirName);
-            case 9:
+            case 8:
                 std::cout << "Enter the directory that will be renamed: ";
                 std::cin >> dirName;
                 std::cout << "Enter the new name for the directory: ";
                 std::cin >> newName;
                 fileSystem.renameDirectory(dirName, newName);
-            case 10:
+            case 9:
                 fileSystem.copyFile();
-            case 11:
+            case 10:
                 fileSystem.sortFilesInDirectory();
-            case 12:
+            case 11:
                 std::cout << "Exiting..." << std::endl;
                 break;
             default:
@@ -119,7 +107,7 @@ int main() {
                 break;
         }
 
-    } while (choice != 12);
+    } while (choice != 11);
 
     return 0;
 }
